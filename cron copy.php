de6d5ac4ddea737abeb6b49973ad365def238416
Hari <hari@Haris-Mac-mini.local>
2025-12-23 12:11:50 +0530

    check localhost

---
+++ cron copy.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "Cron executed at " . date('Y-m-d H:i:s') . "\n";
+
+$data = [
+    "app_id" => $_ENV['ONESIGNAL_APP_ID'],
+    "included_segments" => ["All"],
+    "headings" => ["en" => "Hello"],
+    "contents" => ["en" => "testing cron jobs"],
+];
+
+
+$ch = curl_init("https://onesignal.com/api/v1/notifications");
+
+curl_setopt_array($ch, [
+    CURLOPT_POST => true,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json",
+        "Authorization: Basic " . $_ENV['ONESIGNAL_API_KEY']
+    ],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => json_encode($data),
+]);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo "HTTP: $httpCode\n";
+echo "Response: $response\n";
+
+
+echo "Push sent successfully";
--- cron.php
+++ cron.php
@@ -2,32 +2,16 @@
 
 echo "Cron executed at " . date('Y-m-d H:i:s') . "\n";
 
-$data = [
-    "app_id" => $_ENV['ONESIGNAL_APP_ID'],
-    "included_segments" => ["All"],
-    "headings" => ["en" => "Hello"],
-    "contents" => ["en" => "testing cron jobs"],
-];
 
-
-$ch = curl_init("https://onesignal.com/api/v1/notifications");
-
-curl_setopt_array($ch, [
-    CURLOPT_POST => true,
-    CURLOPT_HTTPHEADER => [
-        "Content-Type: application/json",
-        "Authorization: Basic " . $_ENV['ONESIGNAL_API_KEY']
-    ],
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POSTFIELDS => json_encode($data),
-]);
-
-$response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-echo "HTTP: $httpCode\n";
-echo "Response: $response\n";
-
-
-echo "Push sent successfully";
+$db_server = "127.0.0.1:3306";
+$db_user = "root";
+$db_pass = "";
+$db_name = "360clouderp";
+$conn = "";
+$conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+if ($conn) {
+    echo "You are connected !";
+    
+}else {
+    echo "Could not connect";
+}
