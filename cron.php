bcb5a3d2edb0213a3d33db3b3f955611bc1e5429
Hari <hari@192.168.1.5>
2025-12-23 16:41:38 +0530

    checking1

--- cron.php
+++ cron.php
@@ -2,12 +2,16 @@
 echo "Cron executed at " . date('Y-m-d H:i:s') . "\n";
 
 $playerId = "8065122c-9619-4d81-b01c-595e73f54f8f";
-$ch = curl_init("https://onesignal.com/api/v1/players/$playerId?app_id=" . $_ENV['ONESIGNAL_APP_ID']);
+$appId = $_ENV['ONESIGNAL_APP_ID'];
+$apiKey = $_ENV['ONESIGNAL_API_KEY'];
+
+$ch = curl_init("https://onesignal.com/api/v1/players/$playerId?app_id=$appId");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Authorization: Basic " . $_ENV['ONESIGNAL_API_KEY']
+    "Authorization: Basic $apiKey"
 ]);
-$resp = curl_exec($ch);
+$response = curl_exec($ch);
 curl_close($ch);
-print_r(json_decode($resp, true));
+
+print_r(json_decode($response, true));
 
