37602cb70916d9f726b1078910449c11d7c684c7
Hari <hari@192.168.1.5>
2025-12-23 15:20:41 +0530

    create public url via ngrok

--- cron.php
+++ cron.php
@@ -1,23 +1,42 @@
 <?php
-
 echo "Cron executed at " . date('Y-m-d H:i:s') . "\n";
 
+$ch = curl_init("https://tropologic-reece-nonodorously.ngrok-free.dev/testCron.php");
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+]);
+$response = curl_exec($ch);
+curl_close($ch);
 
-$db_server = "127.0.0.1:3306";
-$db_user = "root";
-$db_pass = "";
-$db_name = "360clouderp";
-$conn = "";
-try {
-    $conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-    if ($conn) {
-    echo "You are connected !";
-    
-}else {
-    echo "Could not connect";
-}
-} catch (\Throwable $th) {
-    echo "not print";
+$playerIds = json_decode($response, true);
+
+if (!$playerIds || count($playerIds) === 0) {
+    echo "No notifications to send.\n";
+    exit;
 }
 
+$data = [
+    "app_id" => $_ENV['ONESIGNAL_APP_ID'],
+    "include_player_ids" => $playerIds,
+    "headings" => ["en" => "Reminder"],
+    "contents" => ["en" => "You have a task reminder now!"],
+];
 
+$ch = curl_init("https://onesignal.com/api/v1/notifications");
+curl_setopt_array($ch, [
+    CURLOPT_POST => true,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json",
+        "Authorization: Basic " . $_ENV['ONESIGNAL_API_KEY']
+    ],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => json_encode($data),
+]);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo "HTTP: $httpCode\n";
+echo "Response: $response\n";
+echo "Push sent successfully\n";
